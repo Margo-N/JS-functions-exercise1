4d8ee8ce24f54c4832418e91001ad8291f842a3f
Margo N <margoneff3@gmail.com>
2024-10-22 05:41:14 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var shoutIt = function (name) {
+  var shout = `Hello, ${name}, nice to meet you!`.toUpperCase();
+  return shout;
+};
+
+console.log(shoutIt("Margo"));
+console.log(shoutIt("Kristen"));
+console.log(shoutIt("Reggie"));
